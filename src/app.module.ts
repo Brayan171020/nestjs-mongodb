7150b156c37c866dc6aa34d93af68d18c4ae3956
Brayan171020 <brayangt1710@gmail.com>
2023-10-18 22:29:46 -0400

    adding env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,16 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     MongooseModule.forRoot(
-      'mongodb+srv://bjgamboa19:f371WyQKL3AGRFZ3@cluster0.yx6ewpj.mongodb.net/bjgamboadb?retryWrites=true&w=majority',
+      `mongodb+srv://${process.env.MONGODB_USER}:${process.env.MONGODB_PASS}@${process.env.MONGODB_CLUSTER}.yx6ewpj.mongodb.net/${process.env.MONGODB_DATABASE}?retryWrites=true&w=majority`,
     ),
     TodoModule,
   ],
